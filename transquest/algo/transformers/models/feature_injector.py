5901968f5b8a4bcaa627880e59726760eddcf1be
m_fomicheva <mari.fomicheva@gmail.com>
2020-05-27 21:31:20 +0200

    Removed non-linearity from the last dense layer

--- transquest/algo/transformers/models/feature_injector.py
+++ transquest/algo/transformers/models/feature_injector.py
@@ -45,7 +45,6 @@ class Reduce(Combinator):
         assert x.shape == (batch_dim, self.num_features)
         x = torch.cat((x, features_inject), dim=1)
         x = self.dense(x)
-        x = torch.tanh(x)
         x = self.out_proj(x)
         return x
 
@@ -62,7 +61,6 @@ class Concat(Combinator):
         assert features_inject.shape[1] == self.num_features
         x = torch.cat((x, features_inject), dim=1)
         x = self.dense(x)
-        x = torch.tanh(x)
         x = self.out_proj(x)
         return x
 
