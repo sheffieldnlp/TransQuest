737dccb12521e4ee969c660858d1da6b1e2e6183
m_fomicheva <mari.fomicheva@gmail.com>
2020-07-02 17:48:29 +0200

    Avoid looking for cache directory if `no_cache=True`

--- transquest/data/dataset.py
+++ transquest/data/dataset.py
@@ -61,14 +61,16 @@ class Dataset:
             os.makedirs(self.config['cache_dir'], exist_ok=True)
 
         mode = 'dev' if self.evaluate else 'train'
-        cached_features_file = os.path.join(
-            self.config['cache_dir'],
-            'cached_{}_{}_{}_{}_{}'.format(
-                mode, self.config['model_type'], self.max_seq_length, self.config['num_labels'], len(self.examples),
-            ),
-        )
+        cached_features_file = None
+        if not no_cache:
+            cached_features_file = os.path.join(
+                self.config['cache_dir'],
+                'cached_{}_{}_{}_{}_{}'.format(
+                    mode, self.config['model_type'], self.max_seq_length, self.config['num_labels'], len(self.examples),
+                ),
+            )
 
-        if os.path.exists(cached_features_file) and (
+        if cached_features_file is not None and os.path.exists(cached_features_file) and (
                 (not self.config['reprocess_input_data'] and not no_cache) or (
                 mode == 'dev' and self.config['use_cached_eval_features'] and not no_cache)
         ):
