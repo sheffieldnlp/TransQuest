6a1064942b7567605a7d81cb452d483914dc6df3
m_fomicheva <mari.fomicheva@gmail.com>
2020-07-02 17:45:10 +0200

    Update configuration after loading it from file only if output directory is provided

--- transquest/data/load_config.py
+++ transquest/data/load_config.py
@@ -7,10 +7,13 @@ from multiprocessing import cpu_count
 def load_config(args):
     config = json.load(open(args.config))
     process_count = cpu_count() - 2 if cpu_count() > 2 else 1
+    if args.output_dir is not None:
+        config.update({
+            'output_dir': os.path.join(args.output_dir, 'outputs'),
+            'cache_dir': os.path.join(args.output_dir, 'cache_dir'),
+            'best_model_dir': os.path.join(args.output_dir, 'best_model'),
+        })
     config.update({
-        'output_dir': os.path.join(args.output_dir, 'outputs'),
-        'best_model_dir': os.path.join(args.output_dir, 'best_model'),
-        'cache_dir': os.path.join(args.output_dir, 'cache_dir'),
         'process_count': process_count,
     })
     return config
