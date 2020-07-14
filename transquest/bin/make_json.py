d5101886503e0a6e64fbb0f6e41a3089524c16e4
m_fomicheva <mari.fomicheva@gmail.com>
2020-07-14 19:33:35 +0200

    Mixing both src and tgt

--- transquest/bin/make_json.py
+++ transquest/bin/make_json.py
@@ -5,11 +5,13 @@ import pandas as pd
 import random
 
 
-def main(inpf, outf, mix=False):
+def main(inpf, outf, mix_src=False, mix_tgt=False):
     data = pd.read_csv(inpf, sep='\t', quoting=3)
     src = data['original']
     tgt = data['translation']
-    if mix:
+    if mix_src:
+        random.shuffle(src)
+    if mix_tgt:
         random.shuffle(tgt)
     data_list = []
     for srci, tgti in zip(src, tgt):
@@ -26,6 +28,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', required=True)
     parser.add_argument('-o', '--output', required=True)
-    parser.add_argument('--mix', default=False, action='store_true')
+    parser.add_argument('--mix_src', default=False, action='store_true')
+    parser.add_argument('--mix_tgt', default=False, action='store_true')
     args = parser.parse_args()
-    main(args.input, args.output, mix=args.mix)
+    main(args.input, args.output, mix_src=args.mix_src, mix_tgt=args.mix_tgt)
