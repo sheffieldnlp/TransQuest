372a8b564f380b2a2a8a41be8662852e0488fd51
m_fomicheva <mari.fomicheva@gmail.com>
2020-12-06 19:19:08 +0100

    Update datasets loader directory

--- transquest_cli/utils.py
+++ transquest_cli/utils.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 # Paths
 PACKAGE_DIR = Path(__file__).resolve().parent.parent
-DATASETS_LOADERS_DIR = PACKAGE_DIR / "datasets"
+DATASETS_LOADERS_DIR = PACKAGE_DIR / "datasets_"
