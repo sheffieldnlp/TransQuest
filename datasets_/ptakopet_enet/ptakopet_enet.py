72145031debaf666d9a8a818b2501f688c4edc7c
m_fomicheva <mari.fomicheva@gmail.com>
2020-12-06 19:30:18 +0100

    Labels are integers

--- datasets_/ptakopet_enet/ptakopet_enet.py
+++ datasets_/ptakopet_enet/ptakopet_enet.py
@@ -50,7 +50,7 @@ class PtakopetEnEt(datasets.GeneratorBasedBuilder):
                 {
                     "src": datasets.Sequence(datasets.Value("string")),
                     "mt": datasets.Sequence(datasets.Value("string")),
-                    "mt_tags": datasets.Sequence(datasets.features.ClassLabel(names=["OK", "BAD"])),
+                    "mt_tags": datasets.Sequence(datasets.features.ClassLabel.str2int()),
                 }
             ),
             supervised_keys=None,
