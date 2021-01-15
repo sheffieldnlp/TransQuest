50d4290f25cae8f46e44ae15d67ef545c94a2d54
feralvam <feralvam@gmail.com>
2021-01-15 15:01:49 +0000

    changed setup

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="transquest",
+    name="deepquest-py",
     version="0.1.0",
-    author="Tharindu Ranasinghe",
-    author_email="rhtdranasinghe@gmail.com",
-    description="Transformer based translation quality estimation",
+    author="SheffieldNLP",
+    description="Pytorch version of DeepQuest - a library for Quality Estimation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sheffieldnlp/TransQuest",
+    url="https://github.com/sheffieldnlp/deepQuest-py",
     packages=find_packages(),
     classifiers=[
         "Intended Audience :: Science/Research",
@@ -22,5 +21,5 @@ setup(
     ],
     python_requires=">=3.6",
     install_requires=["numpy", "tqdm", "transformers", "scipy", "scikit-learn", "pandas", "tokenizers", "datasets",],
-    entry_points={"console_scripts": ["tq-run-word=transquest_cli.run_wordlevel:main",]},
+    entry_points={"console_scripts": ["deepquestpy-run-model=deepquestpy_cli.run_model:main"]},
 )
